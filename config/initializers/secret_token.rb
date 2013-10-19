592690ab07b188b61b4471e662af02f9990f449f
Travis Peterson <tekman82@yahoo.com>
2013-10-19 14:56:40 -0500

    Trashsite Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trashsite::Application.config.secret_key_base = '93e88a0e04433eb515d7b2ce8611177b8e4c337141b9f76841b33968d7e791e3a0e4766cd02c92df782bdd8e0a1c43e847537358e929a80cf9b2cd76990df64f'
